6075be50fd39e4db3bc5b5c37820ef7cce5a44bf
TranThinh112 <ttthinh2005@gmail.com>
2026-06-24 02:00:53 +0700

    upload database

--- BunnyChat/obj/Debug/net8.0/BunnyChat.AssemblyInfo.cs
+++ BunnyChat/obj/Debug/net8.0/BunnyChat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BunnyChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f0319b7fd97e7c73dea40bfa57a914f35d17023")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+889cd772d86ae887e9f84f1ffcf81b3b0d44c532")]
 [assembly: System.Reflection.AssemblyProductAttribute("BunnyChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BunnyChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
